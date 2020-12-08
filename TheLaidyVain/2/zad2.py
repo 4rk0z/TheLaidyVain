c2f0e200bb0a0d38de7892eaee1c373b1a675d37
arkoz <75336759+4rk0z@users.noreply.github.com>
2020-12-08 22:24:58 +0100

    Update zad2.py

--- TheLaidyVain/2/zad2.py
+++ TheLaidyVain/2/zad2.py
@@ -17,14 +17,10 @@ try:
                 byte = f.read(1)
                 if hex(ord(byte)) >= '0xb0':
                     tab.append('1')
-    znak = '0b'  # prefix binarny system
+    znak = '0b'
     for elem in tab:
         znak = znak + elem
-        if int(
-                znak,
-                2) >= 32 and int(
-                znak,
-                2) <= 126 and len(znak) == 10:  # '0b' + 8 bitów
+        if int(znak,2) >= 32 and int(znak,2) <= 126 and len(znak) == 10:
             print(chr(int(znak, 2)), end='', flush=True)
             znak = '0b'
 finally:
